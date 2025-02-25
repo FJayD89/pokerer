e3aafe694685a02266fd40184bff7e2583c9514b
FJayD89 <filipajayd@gmail.com>
2025-02-25 00:29:02 +0000

    rename bits*

---
+++ pokerer.hs
@@ -0,0 +1,19 @@
+import Helpers
+import Hand
+import Card   
+   
+handRF = [Card Ten Spades, Card King Spades, Card Jack Spades, Card Ace Spades, Card Queen Spades]
+handSF		= map (\f -> Card f Spades) fs
+hand4oak	= (map (Card King) (fullRange :: [Suit])) ++ []
+handFH		= map card_tuple (cart [King, Two, Seven] [Diamonds, Hearts, Clubs])
+handStraight	= map card_tuple (zip fs ss)
+handFlush	= map (card_tuple . (,Spades)) [Two, Six, Ace, Five, Jack] 
+hand3oak	= map (card_tuple . (Ten, )) [Diamonds, Hearts, Clubs] ++ [Card Five Clubs]
+hand2p		= map card_tuple (cart [Ten, Jack] [Spades, Diamonds]) 
+handPair	= (Card Four Clubs) : (tail $ map card_tuple (zip fs ss))
+
+
+ss = [Spades, Spades, Diamonds, Clubs, Spades]
+fs = [Five, Three, Four, Seven, Six]
+
+main = interact $ show . mhand . (map cardreader) . words
